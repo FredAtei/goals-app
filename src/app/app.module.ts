d187ead453bd633501b6b545872df8dc223e00ce
Fred <akamaatei@gmail.com>
2020-09-30 09:15:16 +0300

    add  changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GoalComponent } from './goal/goal.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
+import { StrikethroughDirective } from './strikethrough.directive';
+import { DateCountPipe } from './date-count.pipe';
+import { GoalFormComponent } from './goal-form/goal-form.component';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GoalComponent,
+    GoalDetailComponent,
+    StrikethroughDirective,
+    DateCountPipe,
+    GoalFormComponent
   ],
+
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
---
+++ src/app/goal/goal.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AlertService } from '../alert-service/alert.service';
+import { Quote } from '../quote-class/quote';
+import { Goal } from '../goal';
+import { GoalService } from '../goal-service/goal.service';
+
+@Component({
+  selector: 'app-goal',
+  templateUrl: './goal.component.html',
+  styleUrls: ['./goal.component.css']
+})
+export class GoalComponent implements OnInit {
+
+
+  goals: Goal[];
+  alertService: AlertService;
+  quote: Quote;
+
+  deleteGoal(isComplete, index) {
+    if (isComplete) {
+      let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
+      if (toDelete) {
+        this.goals.splice(index, 1)
+        this.alertService.alertMe("The goal had been deleted")
+      }
+    }
+  }
+
+  toggleDetails(index) {
+    this.goals[index].showDescription = !this.goals[index].showDescription;
+  }
+
+  completeGoal(isComplete, index) {
+    if (isComplete) {
+      this.goals.splice(index, 1);
+    }
+  }
+
+  addNewGoal(goal) {
+    let goalLength = this.goals.length;
+    goal.id = goalLength + 1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
+
+  constructor(goalService: GoalService, alertService: AlertService, private http: HttpClient) {
+    this.goals = goalService.getGoals()
+    this.alertService = this.alertService;
+  }
+
+  ngOnInit() {
+
+    interface ApiResponse {
+      author: string;
+      quote: string;
+    }
+
+    this.http.get<ApiResponse>("http://quotes.stormconsultancy.co.uk/random.json").subscribe(data => {
+      // Succesful API request
+      this.quote = new Quote(data.author, data.quote)
+    }), err => {
+      this.quote = new Quote("Winston Churchill", "Never never give up!")
+      console.log("An error occurred")
+    }
+  }
+
+}
